e759906b04ee40f629692b3fbe99e34777a52e71
Loic Johnson <ljohnson@z4r10p5.42lyon.fr>
2022-04-23 11:58:18 +0200

    working on new version of Bronze code

--- Bronze_league_main.c
+++ Bronze_league_main.c
@@ -11,207 +11,98 @@
 typedef struct s_base	t_base;
 typedef struct s_hero	t_hero;
 typedef struct s_entity	t_entity;
+typedef struct s_pos	t_pos;
 
 struct s_base
 {
 	int		entity_count;
-	int		x; //x of base
-	int		y; //y of base
-	int		heroes_count; //number of heroes
-	int		health; //health of base
-	int		e_x; //x of enemy base
-	int		e_y; //y of enemy base
-	int		corner; //0 if ally base is top left, 1 if ally base is bottom right
-	int		time; //time since start
+	int		x;
+	int		y;
+	int		heroes_per_player;
+	int		mana;
+	int		health;
+	int		ey;
+	int		ex;
+	int		time;
+	int		corner;
+	int		radius;
 };
 
 struct s_hero
 {
-	int	id; //id of hero
-	int	x; //x of hero
-	int	y; //y of hero
-	int	mana; //mana of hero
-	int	shield_life; //shield_life of hero
+	int		id;
+	int		shield_life;
+	int		radius;
+	t_pos	pos;
 };
 
 struct s_entity
 {
-	int	id; //Unique identifier for each entity
-	int	type; //0 = monster, 1 = ally hero, 2 = enemy hero
-	int	x; //x of entity
-	int	y; //y of entity
-	int	shield_life; //count down until shield spell fades
-	int	is_controlled; //1 if controlled by a spell, 0 otherwise
-	int	health; //remaining health
-	int	vx; //direction x of entity
-	int	vy; //direction y of entity
-	int	near_base; //0 if no target, 1 if targeting a base
-	int	threat_for; //0 if not a threat, 1 if threat for ally base, 2 if threat for enemy base
+	int		id;
+	int		type;
+	int		shield_life;
+	int		is_controlled;
+	int		max_health;
+	int		health;
+	int		near_base;
+	int		threat_for;
+	t_pos	pos;
 };
 
-int	square(int nb)
+struct s_pos
 {
-	return (nb * nb);
-}
+	int		x;
+	int		y;
+	int		vx;
+	int		vy;
+	int		next_x;
+	int		next_y;
+};
 
-int	square_distance(int	target_x, int target_y, int mark_x, int mark_y)
+void	init_heroes_position(t_base base, t_hero **hero)
 {
-	int	distance_x = square(target_x - mark_x);
-	int	distance_y = square(target_y - mark_y);
-	return (distance_x + distance_y);
-}
-
-int	is_in_range(int target_x, int target_y, int mark_x, int mark_y, int radius)
-{
-	float	distance;
-	float	distance_sqrt;
-
-	distance_sqrt = sqrtf(square_distance(target_x, target_y, mark_x, mark_y));
-	distance = distance_sqrt - radius;
-	if (distance <= 0)
-		return (1);
-	return (0);
-}
-
-// float	get_distance(int target_x, int target_y, int mark_x, int mark_y, int radius)
-// {
-// 	float	distance_sqrt;
-// 	float	distance;
-
-// 	distance_sqrt = sqrtf(square_distance(target_x, target_y, mark_x, mark_y));
-// 	distance = distance_sqrt - radius;
-// 	return (distance);
-// }
-
-// int	get_closest_to_base(t_base *base, t_entity *entity_one, t_entity *entity_two)
-// {
-// 	float	distance_one = get_distance(entity_one->x, entity_one->y, base->x, base->y, 5000);
-// 	float	distance_two = get_distance(entity_two->x, entity_two->y, base->x, base->y, 5000);
-
-// 	if (distance_one <= distance_two)
-// 		return (entity_one->id);
-// 	return (entity_two->id);
-// }
-
-int	move_to_base(t_base *base, int x, int y)
-{
-	int	dest_x;
-	int	dest_y;
-
-	if (!base->corner)
+	if (!base.corner)
 	{
-		dest_x = base->x + x;
-		dest_y = base->y + y;
+		hero[0]->pos.x = 1131;
+		hero[0]->pos.y = 1131;
+		hero[1]->pos.x = 1414;
+		hero[1]->pos.y = 849;
+		hero[2]->pos.x = 849;
+		hero[2]->pos.y = 1414;
 	}
 	else
 	{
-		dest_x = base->x - x;
-		dest_y = base->y - y;
+		hero[0]->pos.x = 16499;
+		hero[0]->pos.y = 7869;
+		hero[1]->pos.x = 16216;
+		hero[1]->pos.y = 8151;
+		hero[2]->pos.x = 16781;
+		hero[2]->pos.y = 7586;
 	}
-	return (printf("MOVE %d %d GRRR\n", dest_x, dest_y));
+	hero[0]->pos.next_x = -1;
+	hero[0]->pos.next_y = -1;
+	hero[1]->pos.next_x = -1;
+	hero[1]->pos.next_y = -1;
+	hero[2]->pos.next_x = -1;
+	hero[2]->pos.next_y = -1;
 }
 
-int	move_to_enemy_base(t_base *base, int x, int y)
+void	init_base_position(t_base *base)
 {
-	int	dest_x;
-	int	dest_y;
-
-	if (!base->corner)
+	if (base->x == 0 && base->y == 0)
 	{
-		dest_x = base->e_x - x;
-		dest_y = base->e_y - y;
+		base->ex = WIDTH;
+		base->ey = HEIGHT;
+		base->corner = 0;
 	}
 	else
 	{
-		dest_x = base->e_x + x;
-		dest_y = base->e_y + y;
+		base->ex = 0;
+		base->ey = 0;
+		base->corner = 1;
 	}
-	return (printf("MOVE %d %d GRRR\n", dest_x, dest_y));
-}
-
-int	count_entity_in_range(t_base *base, t_entity *entity, int mark_x, int mark_y, int radius)
-{
-	int	in_range = 0;
-
-	for (int i = 0; i < base->entity_count; i++)
-	{
-		if (entity[i].type == 0 || entity[i].type == 2)
-		{
-			if (is_in_range(entity[i].x, entity[i].y, mark_x, mark_y, radius))
-				in_range++;
-		}
-	}
-	return (in_range);
-}
-
-int	hero_zero(t_base *base, t_entity *entity, t_hero *hero)
-{
-	if (count_entity_in_range(base, entity, hero->x, hero->y, 1280) >= 2 && hero->mana > 10)
-		return (printf("SPELL WIND %d %d OUAF\n", base->e_x, base->e_y));
-	for (int i = 0; i < base->entity_count; i++)
-	{
-		if (entity[i].type == 0 && is_in_range(entity[i].x, entity[i].y, base->x, base->y, 5000))
-		{
-			if (entity[i].type == 0 && is_in_range(entity[i].x, entity[i].y, base->x, base->y, 2500) && is_in_range(entity[i].x, entity[i].y, hero->x, hero->y, 1280) && hero->mana > 10)
-				return (printf("SPELL WIND %d %d OUAF\n", base->e_x, base->e_y));
-			return (printf("MOVE %d %d GRRR\n", entity[i].x, entity[i].y));
-		}
-		else if (entity[i].type == 2)
-		{
-			if (is_in_range(entity[i].x, entity[i].y, hero->x, hero->y, 1280) && hero->mana > 10)
-				return (printf("SPELL WIND %d %d OUAF\n", base->e_x, base->e_y));
-		}
-	}
-	return (move_to_base(base, 1500, 1500));
-}
-
-int	hero_one(t_base *base, t_entity *entity, t_hero *hero)
-{
-	if (count_entity_in_range(base, entity, hero->x, hero->y, 1280) >= 2 && hero->mana > 10)
-		return (printf("SPELL WIND %d %d OUAF\n", base->e_x, base->e_y));
-	for (int i = 0; i < base->entity_count; i++)
-	{
-		if (entity[i].type == 0 && is_in_range(entity[i].x, entity[i].y, base->x, base->y, 5000))
-		{
-			if (is_in_range(entity[i].x, entity[i].y, base->x, base->y, 2500) && is_in_range(entity[i].x, entity[i].y, hero->x, hero->y, 1280) && hero->mana > 10)
-				return (printf("SPELL WIND %d %d OUAF\n", base->e_x, base->e_y));
-			return (printf("MOVE %d %d GRRR\n", entity[i].x, entity[i].y));
-		}
-		else if (entity[i].type == 2)
-		{
-			if (is_in_range(entity[i].x, entity[i].y, hero->x, hero->y, 1280) && hero->mana > 10)
-				return (printf("SPELL WIND %d %d OUAF\n", base->e_x, base->e_y));
-		}
-	}
-	return (move_to_base(base, 3500, 3500));
-}
-
-int hero_two(t_base *base, t_entity *entity, t_hero *hero)
-{
-	if (!is_in_range(hero->x, hero->y, base->e_x, base->e_y, 6000))
-		return (move_to_enemy_base(base, 3500, 3500));
-	for (int i = 0; i < base->entity_count; i++)
-	{
-		if (entity[i].type == 0)
-		{
-			if (count_entity_in_range(base, entity, hero->x, hero->y, 1280) >= 2 && hero->mana > 10)
-				return (printf("SPELL WIND %d %d OUAF\n", base->e_x, base->e_y));
-			else if (is_in_range(entity[i].x, entity[i].y, hero->x, hero->y, 1280) && hero->mana > 10)
-				return (printf("SPELL WIND %d %d OUAF\n", base->e_x, base->e_y));
-			else if (is_in_range(entity[i].x, entity[i].y, hero->x, hero->y, 2200))
-				return (printf("MOVE %d %d GRRR\n", entity[i].x, entity[i].y));
-		}
-		if (entity[i].type == 2)
-		{
-			if (count_entity_in_range(base, entity, hero->x, hero->y, 1280) >= 2 && hero->mana > 10)
-				return (printf("SPELL WIND %d %d OUAF\n", base->x, base->y));
-			else if (is_in_range(entity[i].x, entity[i].y, hero->x, hero->y, 1280) && hero->mana > 10)
-				return (printf("SPELL WIND %d %d OUAF\n", base->x, base->y));
-			else if (is_in_range(entity[i].x, entity[i].y, hero->x, hero->y, 2200))
-				return (printf("MOVE %d %d GRRR\n", entity[i].x, entity[i].y));
-		}
-	}
-	return (move_to_enemy_base(base, 3500, 2500));
+	base->time = 0;
+	base->radius = 5000;
 }
 
 int	main(void)
@@ -219,63 +110,38 @@ int	main(void)
 	t_base		base;
 	t_entity	*entity;
 	t_hero		*hero;
-	int			b;
+	int			a;
 
-	scanf("%d%d%d", &base.x, &base.y, &base.heroes_count);
-	if (base.x == 0 && base.y == 0)
+	scanf("%d%d%d", &base.x, &base.y, &base.heroes_per_player);
+	init_base_position(&base);
+	hero = malloc(sizeof(t_hero) * base.heroes_per_player);
+	init_heroes_position(base, &hero);
+	while (42)
 	{
-		base.e_x = 17630;
-		base.e_y = 9000;
-		base.corner = 0;
-	}
-	else
-	{
-		base.e_x = 0;
-		base.e_y = 0;
-		base.corner = 1;
-	}
-	hero = malloc(sizeof(t_hero) * base.heroes_count);
-	base.time = 0;
-	while (1)
-	{
-		for (int i = 0; i < 2; i++)
-		{
-			hero[i].id = i;
-			scanf("%d%d", &base.health, &hero[i].mana);
-		}
+		a = 0;
+		for (int i = 0; i < 2; i++) //base initialization
+			scanf("%d%d", &base.health, &base.mana);
 		scanf("%d", &base.entity_count);
-		entity = malloc(sizeof(t_entity) * base.entity_count);
-		b = 0;
-		for (int i = 0; i < base.entity_count; i++)
+		for (int i = 0; i < base.entity_count; i++) //entity initialization
 		{
 			scanf("%d%d", &entity[i].id, &entity[i].type);
-			scanf("%d%d", &entity[i].x, &entity[i].y);
+			scanf("%d%d", &entity[i].pos.x, &entity[i].pos.y);
 			scanf("%d%d%d", &entity[i].shield_life, &entity[i].is_controlled, &entity[i].health);
-			scanf("%d%d", &entity[i].vx, &entity[i].vy);
+			scanf("%d%d", &entity[i].pos.vx, &entity[i].pos.vy);
 			scanf("%d%d", &entity[i].near_base, &entity[i].threat_for);
-			if (entity[i].type == 1 && b < base.heroes_count)
+			if (entity[i].type == 1)
 			{
-				hero[b].id = entity[i].id;
-				hero[b].x = entity[i].x;
-				hero[b].y = entity[i].y;
-				hero[b].shield_life = entity[i].shield_life;
-				b++;
+				hero[a].id = entity[i].id;
+				hero[a].pos.x = entity[i].pos.x;
+				hero[a].pos.y = entity[i].pos.y;
+				hero[a].shield_life = entity[i].shield_life;
+				hero[a].radius = 2200;
+				a++;
 			}
 		}
-		for (int a = 0; a < base.heroes_count; a++)
+		for (int i = 0; i < base.heroes_per_player; i++)
 		{
-			if (a == 0)
-				hero_zero(&base, entity, &hero[0]);
-			else if (a == 1)
-				hero_one(&base, entity, &hero[1]);
-			else if (a == 2)
-				hero_two(&base, entity, &hero[2]);
-			else
-				printf("WAIT ZZZZ\n");
+			printf("WAIT\n");
 		}
-		free (entity);
-		base.time++;
 	}
-	free (hero);
-	return (0);
 }
