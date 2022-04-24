8dbef2847d523e825f06249691807e5ce1cc4a3a
Loic Johnson <ljohnson@z4r10p5.42lyon.fr>
2022-04-24 11:11:31 +0200

    working on new version

--- Bronze_league_main.c
+++ Bronze_league_main.c
@@ -1,3 +1,7 @@
+/*/////////////////////////////////////////////////////////////////////////////
+		INCLUDES & DEFINES
+*//////////////////////////////////////////////////////////////////////////////
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -13,52 +17,76 @@
 #define MENACE_RADIUS 7500
 #define MOVE_RADIUS 1600
 
+/*/////////////////////////////////////////////////////////////////////////////
+		TYPEDEF & STRUCTURES
+*//////////////////////////////////////////////////////////////////////////////
+
+
 typedef struct s_base	t_base;
 typedef struct s_hero	t_hero;
 typedef struct s_entity	t_entity;
 
+//base and game data
 struct s_base
 {
-	int		entity_count;
-	int		x;
-	int		y;
-	int		heroes_per_player;
-	int		health;
-	int		mana;
-	int		ehealth;
-	int		emana;
-	int		ey;
-	int		ex;
-	int		time;
-	int		corner;
+	int		entity_count; //number of visible entities
+	int		x; //pos x of ally base
+	int		y; //pos y of ally base
+	int		heroes_per_player; //number of heroes available per player
+	int		health; //health of ally base
+	int		mana; //mana of ally base
+	int		ex; //pos x of enemy base
+	int		ey; //pos y of enemy base
+	int		ehealth; //health of enemy base
+	int		emana; //mana of enemy base
+	int		time; //number of turns since start
+	int		corner; //position of ally base : top left = 0; bottom right = 1
 };
 
+//t_hero	*hero = malloc(sizeof(t_hero) * base.heroes_per_player);
 struct s_hero
 {
-	int		id;
-	int		shield_life;
-	int		is_controlled;
-	int		x;
-	int		y;
+	int		id; //entity_id of hero
+	int		shield_life; //countdown for shield : 12 at start, -1 per turn; while > 0 = immune to spells
+	int		is_controlled; //0 if not controlled by a spell; 1 if controlled by a spell
+	int		x; //pos x of hero
+	int		y; //pos y of hero
 };
 
+//t_entity	*entity = malloc(sizeof(t_entity) * base.entity_count);
 struct s_entity
 {
-	int		id;
-	int		type;
-	int		shield_life;
-	int		is_controlled;
-	int		health;
-	int		near_base;
-	int		threat_for;
-	int		x;
-	int		y;
-	int		vx;
-	int		vy;
-	int		nx;
-	int		ny;
+	int		id; //entity id
+	int		type; //entity type : 0 = monster; 1 = ally hero; 2 = enemy hero
+	int		shield_life; //countdown for shield : 12 at start, -1 per turn; while > 0 = immune to spells
+	int		is_controlled; //0 if not controlled by a spell; 1 if controlled by a spell
+	int		health; //current health of entity
+	int		near_base; //0 if not targeting a base; 1 if targeting a base
+	int		threat_for; //depending on trajectory : 0 = not a threat; 1 = threat for ally base; 2 = threat for enemy base
+	int		x; //pos x of entity
+	int		y; //pos y of entity
+	int		vx; //vector x of entity
+	int		vy; //vector y of entity
+	int		nx; //pos x + vector x
+	int		ny; //pos x + vector y
 };
 
+int	count_entity_in_range(t_base *base, t_entity *entity, int mark_x, int mark_y, int radius)
+{
+	int	in_range = 0;
+
+	for (int i = 0; i < base->entity_count; i++)
+	{
+		if (entity[i].type == 0 || entity[i].type == 2)
+		{
+			if (is_in_range(entity[i].x, entity[i].y, mark_x, mark_y, radius))
+				in_range++;
+		}
+	}
+	return (in_range);
+}
+
+//Distance formula
 int	square(int nb)
 {
 	return (nb * nb);
@@ -82,22 +110,9 @@ int	is_in_range(int target_x, int target_y, int mark_x, int mark_y, int radius)
 		return (1);
 	return (0);
 }
+//////////////////////////////
 
-int	count_entity_in_range(t_base *base, t_entity *entity, int mark_x, int mark_y, int radius)
-{
-	int	in_range = 0;
-
-	for (int i = 0; i < base->entity_count; i++)
-	{
-		if (entity[i].type == 0 || entity[i].type == 2)
-		{
-			if (is_in_range(entity[i].x, entity[i].y, mark_x, mark_y, radius))
-				in_range++;
-		}
-	}
-	return (in_range);
-}
-
+//Initialization of data
 void	init_base_position(t_base *base)
 {
 	if (base->x == 0 && base->y == 0)
@@ -124,6 +139,75 @@ void	update_heroes_data(t_base base, t_hero *hero, t_entity entity)
 	hero->shield_life = entity.shield_life;
 	hero->is_controlled = entity.is_controlled;
 }
+//////////////////////////////
+
+//Instructions
+int	move_to_base(t_base *base, int x, int y)
+{
+	if (!base->corner)
+		return (printf("MOVE %d %d BASE\n", base->x + x, base->y + y) * 0);
+	return (printf("MOVE %d %d BASE\n", base->x - x, base->y - y) * 0);
+}
+
+int	move_to_enemy_base(t_base *base, int x, int y)
+{
+	if (!base->corner)
+		return (printf("MOVE %d %d BASE\n", base->ex - x, base->ey - y) * 0);
+	return (printf("MOVE %d %d BASE\n", base->ex + x, base->ey + y) * 0);
+}
+
+int	spell_wind(t_base *base, int x, int y)
+{
+	if (base->mana >= 10)
+	{
+		base->mana -= 10;
+		return (printf("SPELL WIND %d %d OUAF\n", x, y) * 0);
+	}
+	return (1);
+}
+
+int	spell_control(t_base *base, int id, int x, int y)
+{
+	if (base->mana >= 10)
+	{
+		base->mana -= 10;
+		return (printf("SPELL CONTROL %d %d %d WOLOLO\n", id, x, y));
+	}
+	return (1);
+}
+
+int	spell_shield(t_base *base, int id)
+{
+	if (base->mana >= 10)
+	{
+		base->mana -= 10;
+		return (printf("SPELL SHIELD %d MEOW\n", id) * 0);
+	}
+	return (1);
+}
+//////////////////////////////
+
+//Hero zero AI
+void	hero_zero(t_base *base, t_entity *entity, t_hero hero)
+{
+
+}
+//////////////////////////////
+
+//Hero one AI
+void	hero_one(t_base *base, t_entity *entity, t_hero hero)
+{
+
+}
+//////////////////////////////
+
+//Hero two AI
+void	hero_two(t_base *base, t_entity *entity, t_hero hero)
+{
+
+}
+//////////////////////////////
+
 
 int	main(void)
 {
